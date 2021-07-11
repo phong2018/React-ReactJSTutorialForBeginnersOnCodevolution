497fe03720a08919345921a88e5d9adaff5fec1a
githubplg@1 <phong2018@gmail.com>
2021-07-12 05:29:01 +0700

    add: user Callback Refs

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class RefsDemo extends Component {
-    constructor(props) {
-        super(props)    
-        this.inputRef=React.createRef()
-    }
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+    this.cbRef = null;
+    this.setCbRef = (element) => {
+      this.cbRef = element;
+    };
+  }
 
-    componentDidMount(){
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+  componentDidMount() {
+    if (this.cbRef) {
+      this.cbRef.focus();
     }
-    clickHandler= ()  => {
-        alert(this.inputRef.current.value)
-    }
-    
-    render() {
-        return (
-            <div>
-                <input type='text' ref={this.inputRef}/>
-                <bution onClick={this.clickHandler}>Click</bution>
-            </div>
-        )
-    }
+    //this.inputRef.current.focus();
+    // console.log(this.inputRef);
+  }
+  clickHandler = () => {
+    alert(this.inputRef.current.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.inputRef} />
+        <input type="text" ref={this.setCbRef} />
+        <buton onClick={this.clickHandler}>Click</buton>
+      </div>
+    );
+  }
 }
 
-export default RefsDemo
+export default RefsDemo;
