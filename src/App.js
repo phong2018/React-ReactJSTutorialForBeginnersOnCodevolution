f4008435d28caafb5c93a85a4b5fe52f81cb79e4
githubplg@1 <phong2018@gmail.com>
2021-07-12 05:42:13 +0700

    add: Refs with class Component attach Refs to DOM element and classCompoent

--- src/App.js
+++ src/App.js
@@ -22,12 +22,14 @@ import Table from "./components/Table";
 import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RefsDemo/>
+        <FocusInput />
+        {/* <RefsDemo/> */}
         {/* <ParentComp/> */}
         {/* <PureComp/> */}
         {/* <Table/> */}
