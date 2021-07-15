553c7c8279e829d78754208e507303a01fbbf772
githubplg@1 <phong2018@gmail.com>
2021-07-16 06:07:56 +0700

    $14: React Hook - Fetching data with useEffect (p3)

--- src/components/HOOKS/DataFectchingOnePost.js
+++ src/components/HOOKS/DataFectchingOnePost.js
@@ -4,9 +4,13 @@ import React, { useState, useEffect } from "react";
 function DataFectchingOnePost() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res);
         setPost(res["data"]);
@@ -14,10 +18,13 @@ function DataFectchingOnePost() {
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fectch Data
+      </button>
       <div>{post.title}</div>
     </div>
   );
