ee7555d9da6dc4a88b77591b50f0fe66a6c6e05d
githubplg@1 <phong2018@gmail.com>
2021-07-15 07:29:51 +0700

    $8: React Hook - conditionally run effects

--- src/components/HOOKS/ClassCounterOne.js
+++ src/components/HOOKS/ClassCounterOne.js
@@ -5,18 +5,29 @@ class ClassCounterOne extends Component {
 
     this.state = {
       count: 0,
+      name: "",
     };
   }
   componentDidMount() {
     document.title = `Cliked ${this.state.count} times`;
   }
-  componentDidUpdate() {
-    document.title = `Clicked ${this.state.count} times`;
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      console.log("Update document tilte");
+      document.title = `Clicked ${this.state.count} times`;
+    }
   }
 
   render() {
     return (
       <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => {
+            this.setState({ name: e.target.value });
+          }}
+        />
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           Click {this.state.count}
         </button>
