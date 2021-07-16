0be974690b40d5b5f7a6355f958d5d2162290d3b
githubplg@1 <phong2018@gmail.com>
2021-07-16 07:16:51 +0700

    $17: UseContext Hook (p3)

--- src/components/HOOKS/ComponentE.js
+++ src/components/HOOKS/ComponentE.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import ComponentF from "./ComponentF";
+import { userContextFunction, channelContextFunction } from "../../App";
 
 function ComponentE() {
+  const user = useContext(userContextFunction);
+  const channel = useContext(channelContextFunction);
+
   return (
     <div>
-      <ComponentF />
+      {user} - {channel}
     </div>
   );
 }
