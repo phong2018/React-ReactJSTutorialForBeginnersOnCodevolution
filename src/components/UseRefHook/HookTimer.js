e8e8337534c8c422d048c65a033dd7f8e1510b9c
githubplg@1 <phong2018@gmail.com>
2021-07-19 07:22:24 +0700

    $29: useRef Hook (p2)

--- src/components/UseRefHook/HookTimer.js
+++ src/components/UseRefHook/HookTimer.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function HookTimer() {
   const [timer, setTimer] = useState(0);
-
+  const intervalRef = useRef();
   useEffect(() => {
-    const interval = setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setTimer((prevTimer) => prevTimer + 1);
     }, 1000);
 
     return () => {
-      clearInterval(interval);
+      clearInterval(intervalRef.current);
     };
   }, []);
 
   return (
     <div>
       Hook Timer - {timer}{" "}
-      <button onClick={() => clearInterval(interval)}>Clear HookTimer</button>
+      <button onClick={() => clearInterval(intervalRef.current)}>
+        Clear HookTimer
+      </button>
       {/* dont know interval when onClick, because dont know interval => have to use useRef*/}
     </div>
   );
