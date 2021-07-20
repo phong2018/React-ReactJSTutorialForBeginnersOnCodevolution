5842d3da015b0a82cd6023c94d5ad8bf4824e9dd
githubplg@1 <phong2018@gmail.com>
2021-07-21 06:23:50 +0700

    $25: redux mapStateToProps

--- src/components/Redux/Redux.js
+++ src/components/Redux/Redux.js
@@ -6,15 +6,20 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 import HooksCakeContainer from "./HooksCakeContainer";
 import IceCreamContainer from "./IceCreamContainer";
+import NewCakeContainer from "./NewCakeContainer";
+import ItemContainer from "./ItemContainer";
 
 export class Redux extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
+          <ItemContainer cake />
+          <ItemContainer />
           <HooksCakeContainer />
           <CakeContainer />
           <IceCreamContainer />
+          <NewCakeContainer />
         </div>
       </Provider>
     );
