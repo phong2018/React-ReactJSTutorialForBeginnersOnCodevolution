cc26c1bf458fe57aadcb1bbdc1876f55e16cd200
githubplg@1 <phong2018@gmail.com>
2021-07-20 09:02:29 +0700

    $20: React Redux + Hook (p2)

--- src/components/Redux/Redux.js
+++ src/components/Redux/Redux.js
@@ -4,12 +4,14 @@ import CakeContainer from "./CakeContainer";
 import store from "./redux/store";
 
 import { Provider } from "react-redux";
+import HooksCakeContainer from "./HooksCakeContainer";
 
 export class Redux extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
+          <HooksCakeContainer />
           <CakeContainer />
         </div>
       </Provider>
