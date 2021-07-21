22cace402b314a05fc5d778c0138b35e46d8e951
githubplg@1 <phong2018@gmail.com>
2021-07-21 08:11:22 +0700

    $29: redux Thunk get request

--- src/components/Redux/redux/store.js
+++ src/components/Redux/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+// Redux Thunk is middleware that allows you to return functions, rather than just actions, within Redux. This allows for delayed actions, including working with promises. One of the main use cases for this middleware is for handling actions that might not be synchronous, for example, using axios to send a GET request.
+import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
 
 export default store;
